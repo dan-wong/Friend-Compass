4d1c623ad27dd86982de7eaea43af21cdfd7876b
Daniel <dan_wong@outlook.com>
2018-11-05 17:03:38 +1300

    Removed unecessary information. Added buttons

--- app/src/main/java/com/daniel/friendcompass/activities/MainActivity/MainActivity.java
+++ app/src/main/java/com/daniel/friendcompass/activities/MainActivity/MainActivity.java
@@ -9,21 +9,28 @@ import android.graphics.BitmapFactory;
 import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.ResultReceiver;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.daniel.friendcompass.Constants;
 import com.daniel.friendcompass.R;
 import com.daniel.friendcompass.activities.UserActivity.UserActivity;
 import com.daniel.friendcompass.azimuth.AzimuthSensor;
 import com.daniel.friendcompass.location.LocationService;
 import com.daniel.friendcompass.userstore.UserStoreCallback;
 import com.daniel.friendcompass.util.BearingRollingAverage;
+import com.daniel.friendcompass.util.FetchAddressIntentService;
 import com.daniel.friendcompass.util.Util;
 
 import java.util.Arrays;
@@ -41,14 +48,15 @@ import permissions.dispatcher.RuntimePermissions;
 public class MainActivity extends AppCompatActivity implements UserStoreCallback {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    @BindView(R.id.azimuthTextView) TextView azimuthTextView;
     @BindView(R.id.locationTextView) TextView locationTextView;
-    @BindView(R.id.targetLocationTextView) TextView targetLocationTextView;
-    @BindView(R.id.bearingToTextView) TextView bearingToTextView;
     @BindView(R.id.distanceTextView) TextView distanceTextView;
     @BindView(R.id.compassImageView) ImageView compassImageView;
     @BindView(R.id.accuracyImageView)
     ImageView accuracyImageView;
+    @BindView(R.id.friendsBtn)
+    Button friendsBtn;
+    @BindView(R.id.mapBtn)
+    Button mapbtn;
 
     private LocationService locationService;
     private Location location;
@@ -69,14 +77,12 @@ public class MainActivity extends AppCompatActivity implements UserStoreCallback
         targetLocation.setLatitude(-36.950141);
         targetLocation.setLongitude(174.623906);
 
-        targetLocationTextView.setText("Target " + getString(R.string.location_placeholder, targetLocation.getLatitude(), targetLocation.getLongitude()));
-
         MainActivityPermissionsDispatcher.startLocationUpdatesWithPermissionCheck(this);
 
         AzimuthSensor azimuthSensor = new AzimuthSensor(this);
         azimuthSensor.registerListener(this.viewModel);
 
-        findViewById(R.id.nameLayout).setOnClickListener(new View.OnClickListener() {
+        friendsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), UserActivity.class);
@@ -84,20 +90,28 @@ public class MainActivity extends AppCompatActivity implements UserStoreCallback
             }
         });
 
+        mapbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (location == null) return;
+                Uri gmmIntentUri = Uri.parse(getString(R.string.maps_uri, location.getLatitude(), location.getLongitude(), "Daniel Wong"));
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+            }
+        });
+
         final Observer<Double> azimuthObserver = new Observer<Double>() {
             @Override
             public void onChanged(@Nullable Double azimuth) {
-                if (azimuth == null) return;
-                int roundedAzimuth = (int) Math.round(azimuth);
-                azimuthTextView.setText(getString(R.string.azimuth_placeholder, roundedAzimuth));
-
-                if (location == null) return;
+                if (azimuth == null || location == null) return;
                 double relativeBearing = Util.getRelativeBearing(location, targetLocation, azimuth);
                 relativeBearing = Util.normalise(rollingAverage.getAverageBearing(relativeBearing));
 
                 int roundedBearing = (int) Math.round(relativeBearing);
-
-                bearingToTextView.setText(getString(R.string.bearing_placeholder, roundedBearing));
                 compassImageView.setRotation(roundedBearing);
             }
         };
@@ -107,8 +121,11 @@ public class MainActivity extends AppCompatActivity implements UserStoreCallback
             public void onChanged(@Nullable Location newLocation) {
                 if (newLocation == null) return;
                 location = newLocation;
-                locationTextView.setText(getString(R.string.location_placeholder, location.getLatitude(), location.getLongitude()));
-                distanceTextView.setText(getString(R.string.distance_placeholder, Util.distanceBetweenTwoCoordinates(location, targetLocation)));
+//                locationTextView.setText(getString(R.string.location_placeholder, location.getLatitude(), location.getLongitude()));
+                distanceTextView.setText(getString(R.string.distance_placeholder,
+                        Math.round(Util.distanceBetweenTwoCoordinates(location, targetLocation))));
+
+                getLocationAddress(location, new AddressResultReceiver(new Handler(Looper.getMainLooper())));
             }
         };
 
@@ -135,6 +152,13 @@ public class MainActivity extends AppCompatActivity implements UserStoreCallback
         viewModel.getSensorAccuracy().observe(this, sensorAccuracyObserver);
     }
 
+    private void getLocationAddress(Location location, ResultReceiver resultReceiver) {
+        Intent intent = new Intent(this, FetchAddressIntentService.class);
+        intent.putExtra(Constants.RECEIVER, resultReceiver);
+        intent.putExtra(Constants.LOCATION_DATA_EXTRA, location);
+        startService(intent);
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -157,7 +181,6 @@ public class MainActivity extends AppCompatActivity implements UserStoreCallback
         location.setLongitude(Double.valueOf(locationStringTokens.get(1)));
 
         targetLocation = location;
-        targetLocationTextView.setText("Target " + getString(R.string.location_placeholder, targetLocation.getLatitude(), targetLocation.getLongitude()));
 
 //        Toast.makeText(this, "Location Updated!", Toast.LENGTH_SHORT).show();
     }
@@ -196,5 +219,20 @@ public class MainActivity extends AppCompatActivity implements UserStoreCallback
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         MainActivityPermissionsDispatcher
                 .onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    class AddressResultReceiver extends ResultReceiver {
+        AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            if (resultData == null) return;
+            String address = resultData.getString(Constants.RESULT_DATA_KEY);
+            if (resultCode == Constants.SUCCESS_RESULT && address != null) {
+                locationTextView.setText(getString(R.string.location_placeholder, address));
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/daniel/friendcompass/util/FetchAddressIntentService.java
@@ -0,0 +1,75 @@
+package com.daniel.friendcompass.util;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.daniel.friendcompass.Constants;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class FetchAddressIntentService extends IntentService {
+    private static final String TAG = FetchAddressIntentService.class.getSimpleName();
+
+    private ResultReceiver resultReceiver;
+
+    public FetchAddressIntentService() {
+        super("FetchAddressIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent == null) return;
+
+        Location location = intent.getParcelableExtra(Constants.LOCATION_DATA_EXTRA);
+        resultReceiver = intent.getParcelableExtra(Constants.RECEIVER);
+
+        String errorMessage = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = null;
+
+        try {
+            addresses = geocoder.getFromLocation(
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    1);
+        } catch (IOException e) {
+            errorMessage = "Service not available";
+            Log.e(TAG, errorMessage, e);
+        }
+
+        if (addresses == null || addresses.size() == 0) {
+            if (errorMessage.isEmpty()) {
+                errorMessage = "No addresses found";
+                Log.e(TAG, errorMessage);
+            }
+            deliverResultToReceiver(Constants.FAILURE_RESULT, errorMessage);
+        } else {
+            Address address = addresses.get(0);
+            List<String> addressFragments = new ArrayList<>();
+
+            addressFragments.add(address.getFeatureName() + " " + address.getThoroughfare()); //Number and Road
+            addressFragments.add(address.getSubLocality()); //Suburb
+            addressFragments.add(address.getLocality()); //City
+
+            deliverResultToReceiver(Constants.SUCCESS_RESULT,
+                    TextUtils.join(", ", addressFragments));
+        }
+    }
+
+    private void deliverResultToReceiver(int resultCode, String message) {
+        Bundle bundle = new Bundle();
+        bundle.putString(Constants.RESULT_DATA_KEY, message);
+        resultReceiver.send(resultCode, bundle);
+    }
+}
